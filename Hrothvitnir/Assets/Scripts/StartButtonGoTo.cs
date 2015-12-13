d0c5c7000afc3621f96b32e8ab52f6a434b07865
Jacob McFadden <Jmcfadd7@masonlive.gmu.edu>
2015-12-13 13:36:29 -0500

    Added Basic Title Scene and End Screen scene
    End Screen scene contains nothing currently while Title scene has a
    basic layout and a attempt at trying to get a script working for the
    start button.

---
+++ Hrothvitnir/Assets/Scripts/StartButtonGoTo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartButtonGoTo : MonoBehaviour {
+
+    public void LoadScene() {
+        SceneManager.LoadScene("mainScene");
+    }
+
+}
