222fe059b6dd57109dc24845af19aaf8dcfaece6
Jacob McFadden <Jmcfadd7@masonlive.gmu.edu>
2015-12-14 11:09:15 -0500

    Added Health Display
    Edited Player script so health is static, health starts at 3, and if
    health reaches 0 it should start a fail state. Added new script that
    should display hearts based on health and will disable them if loses
    health.

---
+++ Hrothvitnir/Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour {
+
+    public int hearts;
+	// Use this for initialization
+	void Start () {
+
+        hearts = MousePlayerControl.health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        hearts = MousePlayerControl.health;
+
+        if(hearts == 2) {
+            GameObject.Find("Heart (2)").GetComponent<Image>().enabled = false;
+        }
+
+        if (hearts == 1) {
+            GameObject.Find("Heart (1)").GetComponent<Image>().enabled = false;
+        }
+
+        if (hearts == 0) {
+            GameObject.Find("Heart").GetComponent<Image>().enabled = false;
+        }
+    }
+}
