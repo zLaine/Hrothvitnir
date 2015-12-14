b1b54bb38608465bef9181fcbde6415b1b5c7810
Zack Laine <rumtehk_uh@yahoo.com>
2015-12-14 01:14:54 -0500

    Added basic wandering AI
    nyoom

--- Hrothvitnir/Assets/Scripts/WanderAI.cs
+++ Hrothvitnir/Assets/Scripts/WanderAI.cs
@@ -1,18 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class wanderAI : MonoBehaviour {
+public class WanderAI : MonoBehaviour {
 
     Vector3 targetLocation;
     Vector3 currentLocation;
+    public float walkSpeed;
 
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+        targetLocation = new Vector3(Random.Range(-2F, 2.6F), Random.Range(-1F, 2.4F), 0);
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        currentLocation = transform.position;
+
+        if(currentLocation == targetLocation)
+        {
+            targetLocation = new Vector3(Random.Range(-2F, 2.6F), Random.Range(-1F, 2.4F), 0);
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetLocation, walkSpeed);
+
+    }
 }
