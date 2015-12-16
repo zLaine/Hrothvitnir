006a36d6db4d4d1d1346e49ce9a81e26ac7503a3
Jacob McFadden <Jmcfadd7@masonlive.gmu.edu>
2015-12-16 18:31:27 -0500

    Changes in how the Title menu works, now everything is only one scene except for Start Game. The game is under a new scene still. If this is efficient or not, I don't know, but it stops the music from starting over every time someone moves between menu. Game over needs to be fixed still though.

---
+++ Hrothvitnir/Assets/Scripts/UI Scripts/TiltleMenu.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiltleMenu : MonoBehaviour {
+
+    //List of Canvas Objects
+    public GameObject TitleCanvas;
+
+    public GameObject OptionsCanvas;
+    public GameObject ControlsCanvas;
+    public GameObject VideoCanvas;
+    public GameObject AudioCanvas;
+    public GameObject ScoreCanvas;
+    public GameObject CreditCanvas;
+
+
+	// Use this for initialization
+	void Start () {
+
+        TitleCanvas.SetActive(true);
+
+        OptionsCanvas.SetActive(false);
+        ControlsCanvas.SetActive(false);
+        VideoCanvas.SetActive(false);
+        AudioCanvas.SetActive(false);
+        ScoreCanvas.SetActive(false);
+        CreditCanvas.SetActive(false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+    //Makes Title Menu visible when called
+    public void TitleMenu() {
+
+        OptionsCanvas.SetActive(false);
+
+        TitleCanvas.SetActive(true);
+    }
+
+    //Moves to scene with actual gameplay
+    public void StartGame() {
+
+        Application.LoadLevel("mainScene");
+    }
+
+    //Makes Option Menu visible when called
+    public void OptionsMenu() {
+
+        TitleCanvas.SetActive(false);
+
+        ControlsCanvas.SetActive(false);
+        VideoCanvas.SetActive(false);
+        AudioCanvas.SetActive(false);
+
+        OptionsCanvas.SetActive(true);
+    }
+
+    //Makes Control Menu visible when called
+    public void ControlMenu()
+    {
+        OptionsCanvas.SetActive(false);
+
+        ControlsCanvas.SetActive(true);
+    }
+
+    //Makes Video Menu visible when called
+    public void VideoMenu() {
+        OptionsCanvas.SetActive(false);
+
+        VideoCanvas.SetActive(true);
+    }
+
+    //Makes Audio Menu visible when called
+    public void AudioMenu() {
+        OptionsCanvas.SetActive(false);
+
+        AudioCanvas.SetActive(true);
+    }
+
+    public void ScoreMenu() {
+        TitleCanvas.SetActive(false);
+
+        ScoreCanvas.SetActive(true);
+    }
+
+    public void CreditMenu() {
+        TitleCanvas.SetActive(false);
+
+        CreditCanvas.SetActive(true);
+    }
+}
